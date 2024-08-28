ccfd4cc3d19d5da3fb8654a1d9373efb0667f154
Santiago542 <santiagocortes464@gmail.com>
2024-08-28 03:00:36 +0200

    Create buttons listeners and implementoperate, inputNumber, inputOperator and inputEqual functions

---
+++ script.js
@@ -0,0 +1,144 @@
+function operate(firstNumber, secondNumber, operator) {
+    switch (operator) {
+        case "+":
+            return firstNumber + secondNumber;
+        case "-":
+            return firstNumber - secondNumber;
+        case "*":
+            return firstNumber * secondNumber;
+        case "/":
+            return secondNumber === 0 ? "Error" : firstNumber / secondNumber;
+        default:
+            return "Error";
+    }
+}
+
+function updateDisplay() {
+    const display = document.querySelector("#display");
+    display.innerText = displayValue;
+    console.log(firstNumber, firstOperator, secondNumber, secondOperator);
+}
+
+function clearDisplay() {
+    firstNumber = null;
+    secondNumber = null;
+    firstOperator = null;
+    secondOperator = null;
+    result = null;
+    displayValue = "0";
+    updateDisplay();
+}
+
+function inputNumber(number) {
+    if(firstOperator === null) {
+        if(displayValue === "0" || displayValue === 0 || displayValue === firstNumber) {
+            displayValue = number;
+        }else {
+            displayValue += number;
+        }
+    } else {
+        if(displayValue === firstNumber) {
+            displayValue = number;
+        } else {
+            displayValue += number;
+        }
+    } 
+}
+
+function inputOperator(operator) {
+    if(firstOperator != null && secondOperator === null) {
+        secondOperator = operator;
+        secondNumber = displayValue;
+        result = operate(Number(firstNumber), Number(secondNumber), firstOperator);
+        displayValue = result.toString();
+        firstNumber = displayValue;
+        result = null;
+    } else if(firstOperator != null && secondOperator != null) {
+        secondNumber = displayValue;
+        result = operate(Number(firstNumber), Number(secondNumber), secondOperator);
+        secondOperator = operator;
+        displayValue = result.toString();
+        firstNumber = displayValue;
+        result = null;
+    } else { 
+        firstOperator = operator;
+        firstNumber = displayValue;
+    }
+}
+function inputEqual() {
+    if(firstOperator === null) {
+        displayValue = displayValue;
+    } else if(secondOperator !== null) {
+        secondNumber = displayValue;
+        result = operate(Number(firstNumber), Number(secondNumber), secondOperator);
+        if(result === "Error") {
+            displayValue = "Error";
+        } else {
+            displayValue = result.toString();
+            firstNumber = displayValue;
+            secondNumber = null;
+            firstOperator = null;
+            secondOperator = null;
+            result = null;
+        }
+    } else {
+        secondNumber = displayValue;
+        result = operate(Number(firstNumber), Number(secondNumber), firstOperator);
+        if(result === "Error") {
+            displayValue = "Error";
+        } else {
+            displayValue = result.toString();
+            firstNumber = displayValue;
+            secondNumber = null;
+            firstOperator = null;
+            secondOperator = null;
+            result = null;
+        }
+        
+    }
+}
+
+function clickButtons() {
+    const buttons = document.querySelectorAll("button");
+
+    buttons.forEach((button) => {
+        button.addEventListener("click", () => {
+            if(button.classList.contains("number")) {
+                inputNumber(button.value);
+                // Debugging
+                console.log(firstNumber, firstOperator, secondNumber, secondOperator);
+
+                updateDisplay();
+            } else if(button.classList.contains("operator")) {
+                inputOperator(button.value);
+                // Debugging
+                console.log(firstNumber, firstOperator, secondNumber, secondOperator);
+
+                updateDisplay();
+            } else if(button.classList.contains("equal")) {
+                inputEqual();
+                
+                updateDisplay();
+            } else if(button.classList.contains("sign")) {
+                // Implementing at the end
+            } else if(button.classList.contains("decimal")) {
+                // Implementing at the end
+            } else if(button.classList.contains("percent")) {
+                // Implementing at the end
+            } else if(button.classList.contains("clear")) {
+                clearDisplay();
+            }
+        });
+    });
+}
+
+let firstNumber = null;
+let secondNumber = null;
+let firstOperator = null;
+let secondOperator = null;
+let result = null;
+let displayValue = "0";
+
+updateDisplay();
+
+clickButtons();
